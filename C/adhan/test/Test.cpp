deffccbff20a09ca85c7738ae92e6e2f5dd40d2a
radcheb <radcheb@gmail.com>
2017-10-05 00:49:38 +0200

    putenv((char *) TZ=UTC);

--- C/adhan/test/Test.cpp
+++ C/adhan/test/Test.cpp
@@ -3,6 +3,7 @@
 #include "../src/include/prayer_times.h"
 
 int main(int argc, char **argv) {
+    putenv((char *) "TZ=UTC");
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
